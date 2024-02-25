204190d8327d5f0023462f1a4a23a0710af3b5ca
ridhonovembri <ridhonovembri@gmail.com>
2024-02-25 14:28:34 +0700

    unlink route again

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const cors = require("cors")
-const appRoutes = require('./routes/AppRoutes')
+// const appRoutes = require('./routes/AppRoutes')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({ origin: "*" }))
-app.use(appRoutes)
+// app.use(appRoutes)
 
 app.get('/', (req, res) => {
   res.status(200).send('Hi Vercel...!!')
