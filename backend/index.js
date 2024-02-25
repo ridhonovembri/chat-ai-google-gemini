49e3ebdfbd8bf768637bbb20b952c9544104da39
ridhonovembri <ridhonovembri@gmail.com>
2024-02-25 14:16:46 +0700

    remarks routes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({ origin: "*" }))
-app.use(appRoutes)
+// app.use(appRoutes)
 
 app.get('/', (req, res) => {
   res.status(200).send('Hi Vercel...!')
