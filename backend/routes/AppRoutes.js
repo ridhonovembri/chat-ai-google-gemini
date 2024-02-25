0ff4fe2037d134103b121ed9ccf08cb0c38ff843
ridhonovembri <ridhonovembri@gmail.com>
2024-02-25 14:38:41 +0700

    change routes and vercel.json

--- backend/routes/AppRoutes.js
+++ backend/routes/AppRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const route = express.Router()
 const gemini = require('../controllers/index')
 
-route.post('/response', gemini.generateResponse)
-route.post('/chat', gemini.generateChat)
+route.post('/api/response', gemini.generateResponse)
+route.post('/api/chat', gemini.generateChat)
 
 module.exports = route
