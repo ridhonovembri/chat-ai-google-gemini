c1c52a2cb8ffe413e19157fee8b40b81131d0216
ridhonovembri <ridhonovembri@gmail.com>
2024-02-25 08:56:24 +0700

    change baseURL

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -2,7 +2,8 @@ import axios from "axios"
 
 export default axios.create({
   // baseURL: 'https://jsonplaceholder.typicode.com',
-  baseURL: 'http://localhost:3000/',
+  // baseURL: 'http://localhost:3000/',
+  baseURL: 'https://ask-jarvis-be.vercel.app/',
   // baseURL: 'api/',
   headers: {
     'Content-type': 'application/json'
