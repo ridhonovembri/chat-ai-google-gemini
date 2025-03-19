599977a88a5d23e59b35b1d07646bf85b37b0fdf
Novembri <Ridho.Novembri@mcdermott.com>
2025-03-19 11:43:56 +0700

    change api-key

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -9,7 +9,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 //const configuration = new GoogleGenerativeAI('AIzaSyAcBs0xovZfD_-Y7nq2lyuO8xDNBX7V0ms');
-const configuration = new GoogleGenerativeAI('AIzaSyD3UAmrv6PUBRMv4vQ1QWmRp97yeyIUx9s');
+const configuration = new GoogleGenerativeAI('AIzaSyB65FytQSBjU8krFFicqjQVcm5MExgmADc');
+
 
 const modelId = "gemini-pro";
 
